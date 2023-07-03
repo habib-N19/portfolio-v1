4c9b451a99cfb0f15c6038f3a60bdaeff7e33589
habib-N19 <habiburnabiarafat@gmail.com>
2023-07-03 23:41:16 +0600

    added resume download button

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -1,9 +1,15 @@
+import resume from '/resume.pdf'
 const Hero = () => {
   return (
     <header className='grid w-11/12 min-h-[400px] grid-cols-1 gap-3 mx-auto border md:grid-cols-2'>
       <section className='col-span-1 text-center'>
         Hello My Name is Habib and I am a frontend Developer
         {/* TODO:adding two buttons for downloading resume and texting me via email/other relevant medium  */}
+        <button className='btn'>
+          <a href={resume} download={true}>
+            Download Resume
+          </a>
+        </button>
       </section>
       <section>Here will be my image </section>
     </header>
---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,5 @@
+const Projects = () => {
+  return <div>all project are here</div>
+}
+
+export default Projects
