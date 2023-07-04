f28ec38a58bf2ac4d5b0b45a922c3628e222c448
habib-N19 <habiburnabiarafat@gmail.com>
2023-07-04 10:57:26 +0600

    fixed the navbar style and position

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -7,6 +7,10 @@ const NavBar = () => {
       title: 'Home'
     },
     {
+      url: '/about',
+      title: 'About'
+    },
+    {
       url: '/blog',
       title: 'Blogs'
     },
@@ -17,11 +21,14 @@ const NavBar = () => {
   ]
 
   return (
-    <nav className='mb-2 md:mb-6'>
-      <ul className='flex justify-end w-11/12 py-4 mx-auto md:gap-4'>
+    <nav className='mb-2  md:mb-6'>
+      <ul className='flex justify-center px-4 py-3 mx-auto mt-5 font-semibold rounded-full shadow-lg w-fit bg-zinc-800 md:gap-4'>
         {navItems.map(n => (
           <li key={n.url}>
-            <NavLink className='hover:underline' to={n.url}>
+            <NavLink
+              className='transition hover:underline hover:duration-150 hover:opacity-100 hover:text-white'
+              to={n.url}
+            >
               {n.title}
             </NavLink>
           </li>
